efdf0bce5e229a21d4a59dea2319e988c2d5c4a7
Greenka2016 <74435631+Greenka2016@users.noreply.github.com>
2020-12-23 15:25:59 +0500

    Add files via upload

---
+++ 2_19.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, f;
+	cout << "Vvedite kolichestvo slagaemyh: ";
+	cin >> n;
+	int sum = 0;
+	for (int i = 1; i <= n; i++)
+	{
+	    f = (i % 3 != 0);
+		sum = sum + (f * 2 - 1) * i;
+	}
+	cout << sum;
+    return 0;
+}
